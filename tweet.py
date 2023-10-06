e8e480541429a50cbdc363891586a79a8d8174c6
KJ <kernel1983@gmail.com>
2023-10-06 12:22:25 +0800

    update algorithm

--- tweet.py
+++ tweet.py
@@ -5,13 +5,13 @@ def reply(root_tweet, parent_id, reply_tweet):
         root_tweet['replies'].append(reply_tweet)
         return
 
-    tweet = get(root_tweet['replies'], parent_id)
+    tweet = _get(root_tweet['replies'], parent_id)
     #print('reply', tweet)
     tweet.setdefault('replies', [])
     tweet['replies'].append(reply_tweet)
 
 
-def get(replies, reply_id):
+def _get(replies, reply_id):
     for reply in replies:
         #print('reply', reply)
         if reply['id'] == reply_id:
@@ -21,21 +21,48 @@ def get(replies, reply_id):
         if r:
             return r
 
+def reply2(root_tweet, parent_id, reply_tweet):
+
+    #tweet = _get(root_tweet['replies'], parent_id)
+    #print('reply', tweet)
+    #tweet.setdefault('replies', [])
+    #tweet['replies'].append(reply_tweet)
+
+    queue = [root_tweet]
+    while queue:
+        obj = queue.pop(0)
+        #print(obj['id'])
+        if obj['id'] == parent_id:
+            obj.setdefault('replies', [])
+            obj['replies'].append(reply_tweet)
+            return
+
+        for reply in obj.get('replies', []):
+            queue.append(reply)
+
+def load_content(db, tweet_obj):
+    queue = [tweet_obj]
+    while queue:
+        obj = queue.pop(0)
+        print(obj['id'])
+        for reply in obj.get('replies', []):
+            queue.append(reply)
 
 if __name__ == '__main__':
     t = {'id': '1', 'replies': [ {'id':'11', 'replies':[ {'id':'111', 'replies':[]} ]} ]}
-    #print(t)
-
-    #print(get(t, '1'))
-    print(get(t['replies'], '11'))
-    print(get(t['replies'], '111'))
-
-    reply(t, '1', {'id':'12'})
     print(t)
 
-    reply(t, '12', {'id':'121'})
+    #print(_get(t, '1'))
+    #print(_get(t['replies'], '11'))
+    #print(_get(t['replies'], '111'))
+
+    reply2(t, '1', {'id':'12'})
     print(t)
 
-    reply(t, '111', {'id':'1112'})
+    reply2(t, '12', {'id':'121'})
     print(t)
+
+    reply2(t, '111', {'id':'1112'})
+    print(t)
+    #load_content(None, t)
 
